51570169f6adc1885ebb0fd97283ce5cad0dcff9
Trathen231 M <trathen231@gmail.com>
2023-08-19 22:02:09 +0000

    Java Most Common Character

--- src/main/java/MostCommonCharacter.java
+++ src/main/java/MostCommonCharacter.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 
 public class MostCommonCharacter {
     /**
@@ -8,6 +10,37 @@ public class MostCommonCharacter {
      * @return the most common character within str.
      */
     public char recurringChar(String str) {
-        return ' ';
+        if (str == null || str.isEmpty()) {
+            throw new IllegalArgumentException("Input string cannot be null or empty.");
+        }
+
+        Map<Character, Integer> charCountMap = new HashMap<>();
+        
+        // Count occurrences of each character
+        for (char c : str.toCharArray()) {
+            charCountMap.put(c, charCountMap.getOrDefault(c, 0) + 1);
+        }
+        
+        char mostCommonChar = ' ';
+        int maxCount = 0;
+
+        // Find the character with the highest count
+        for (Map.Entry<Character, Integer> entry : charCountMap.entrySet()) {
+            if (entry.getValue() > maxCount) {
+                maxCount = entry.getValue();
+                mostCommonChar = entry.getKey();
+            }
+        }
+
+        return mostCommonChar;
+    }
+
+    public static void main(String[] args) {
+        MostCommonCharacter mostCommonCharFinder = new MostCommonCharacter();
+
+        String input = "hello world";
+        char mostCommonChar = mostCommonCharFinder.recurringChar(input);
+
+        System.out.println("The most common character in \"" + input + "\" is: " + mostCommonChar);
     }
 }
